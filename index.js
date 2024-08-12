5ec7977f43814cec99051691fa5674713d594d5c
Katie Young <katie@sandstone.la>
2024-08-12 14:28:26 -0700

    older dates working, 2024 dates not

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 require('dotenv').config();
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, clé tile!');
-});
+    res.send('Hello, clé tile!');
+  });
 
 app.post('/webhook', async (req, res) => {
     const payload = req.body;
     const { contact_email, propertyName, propertyValue } = payload[0];
     try {
-        const customerRes = await axios.get(`https://${process.env.SHOP_NAME}.myshopify.com/admin/api/2024-01/customers/search.json?query=email:${contact_email}`, {
+        const customerResponse = await axios.get(`https://${process.env.SHOP_NAME}.myshopify.com/admin/api/2023-04/customers/search.json?query=email:${contact_email}`, {
             headers: {
                 'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN
             }
         });
 
-        const customer = customerRes.data.customers[0];
+        const customer = customerResponse.data.customers[0];
 
         if (customer) {
-            const updateRes = await axios.put(`https://${process.env.SHOP_NAME}.myshopify.com/admin/api/2024-01/customers/${customer.id}.json`, {
+            const updateResponse = await axios.put(`https://${process.env.SHOP_NAME}.myshopify.com/admin/api/2023-04/customers/${customer.id}.json`, {
                 customer: {
                     id: customer.id,
                     [propertyName]: propertyValue
